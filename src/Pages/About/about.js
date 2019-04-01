70d2e8abb44b471029fa56758630b496363dbace
ocean <ocean@localhost.localdomain>
2019-04-01 13:24:57 +0100

    add jsx textarea field

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Header from '../../HeaderComponent';
 import Footer from '../../FooterComponent';
+import './about.css';
 
 export default class Business extends React.Component {
     render() {
         return(
             <div>
             <Header/>
-            <h1 className="label">About Page</h1>
+            <h1 className="label">About N-Linx</h1>
             <p>N-Linx is where you get the web links to latest gist!</p>
             <p>It is simply a collection of stories from varied sources which is served via web links</p>
             <p>Just click on any story you fancy, and you shall be directed to its source.You will have to use the "back button" on your device to return to our app though.</p>
             <p>Please, do.</p>
 
             <img src="../../ocean-red.jpg" alt="ocean" id="about-owner"/>
-            <p>It was put together by Ocean Semreh Diaali</p>
+            <p>Architecture by Ocean Semreh Diaali</p>
 
+            <div id="message-box">
+            <h2>Send Ocean a message</h2>
+            <form className="ui-form">
+                <textarea placeholder="type a message.." rows="3"/><br/>
+                <input type="submit" value="Send" id="msg-btn"/>
+            </form>
+            </div>
             <Footer/>
             </div>
         );
