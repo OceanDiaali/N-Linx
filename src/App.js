93f1e1bf41f084f6dc1704c12f3b7321845cf171
ocean <ocean@localhost.localdomain>
2019-04-02 17:43:53 +0100

    add header and footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
 import Home from './Pages/Home/home';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <Home/>
+
       </div>
     );
   }
