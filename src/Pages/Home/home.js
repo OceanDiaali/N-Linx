5034e91097a1d67dc292c35ad97544cdf98de39f
ocean <ocean@localhost.localdomain>
2019-04-03 15:27:21 +0100

    add 2nd card-deck and route link for img

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
@@ -8,31 +8,55 @@ export default class Home extends Component {
                 
                 <div className="jumbotron jumbotron-fluid">
                 <div className="container">
-                  <h1 className="label">Welcome to N-Linx!</h1>      
-                  <p>N-Linx is the home of ALL the news a Nigerian would want.</p>
+                  <h1 className="label">N-Linx!</h1>      
+                  <p>Get all the important, latest news from around the world.</p>
                 </div>
                 </div>
 
                 <div className="card-deck">
                   <div className="card bg-primary">
-                  <img className="card-img-top" src="../../tiwa_savage.jpeg" alt="Gossip"/>
+                  <img className="card-img-top" src="../../beyonce.png" alt="Entertainment"/>
                     <div className="card-body text-center card-img-overlay">
-                      <p className="card-text">Get all the celebrity gossip!</p>
+                      <Link className="card-text" to="/entertainment">Entertainment news and celebrity gossip</Link>
                     </div>
                   </div>
                   <div className="card bg-warning">
-                  <img className="card-img-top" src="../../dubai-airport.jpg" alt="Biz & Tech"></img>
+                  <img className="card-img-top" src="../../cash-wads.png" alt="Biz"></img>
                     <div className="card-body text-center card-img-overlay">
-                      <p className="card-text">Read about all the business and tech!</p>
+                      <Link className="card-text" to="/business">Read about all the business and financials</Link>
                     </div>
                   </div>
                   <div className="card bg-success">
-                  <img className="card-img-top" src="../../lasgidi-map.png" alt="Faces & Places"></img>
+                  <img className="card-img-top" src="../../techo.png" alt="Tech"></img>
                     <div className="card-body text-center card-img-overlay">
-                      <p className="card-text">Find news on travel and destinations</p>
+                      <Link className="card-text" to="/technology">Find news on tech and gadgets</Link>
                     </div>
                 </div>
               </div>  
+
+              <div className="card-deck">
+                {/**second deck */}
+
+                <div className="card bg-warning">
+                  <img className="card-img-top" src="../../doctor.png" alt="Health"/>
+                    <div className="card-body text-center card-img-overlay">
+                      <Link className="card-text" to="/health">Learn the latest in health and wellness</Link>
+                    </div>
+                  </div>
+                  <div className="card bg-success">
+                  <img className="card-img-top" src="../../slamdunk.png" alt="Sports"></img>
+                    <div className="card-body text-center card-img-overlay">
+                      <Link className="card-text" to="/sports">Get all the latest sports stories</Link>
+                    </div>
+                  </div>
+                  <div className="card bg-danger">
+                  <img className="card-img-top" src="../../list.png" alt="List"></img>
+                    <div className="card-body text-center card-img-overlay">
+                      <Link className="card-text" to="/about">See all the news that matter</Link>
+                    </div>
+                </div>
+
+              </div>
 
             </div>
         );
