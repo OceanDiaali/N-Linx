fbbdd39e4c226f8097e605ea35f1005096d3a1cd
ocean <ocean@localhost.localdomain>
2019-04-11 16:45:03 +0100

    replace preview link

--- src/Pages/Chat/chat.js
+++ src/Pages/Chat/chat.js
@@ -7,7 +7,7 @@ export default class Chat extends Component {
         return(
             <div class="container text-center">
               <img src="../../point_finger.png" alt="pointing finger" id="finger"/>
-              <a href="https://assistant-chat-us-east.watsonplatform.net/web/public/6bfb32ca-ecc1-4a6f-82ee-b0d2935b4d1a" target="_blank" rel="noopener noreferrer" class="btn btn-info" role="button" id="chat-btn">Launch Chat</a>
+              <a href="https://assistant-chat-us-east.watsonplatform.net/web/public/6fc09082-b569-47dc-952d-60939d13c2ba" target="_blank" rel="noopener noreferrer" class="btn btn-info" role="button" id="chat-btn">Launch Chat</a>
               <p className="chat-label">The button launches a PREVIEW of the Virtual Assistant in  a new tab or window (depending on your browser configuration)</p>
             </div>
         );
