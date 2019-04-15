dd563a19c171d5cd4cef560ef7e673ad823363bc
ocean <ocean@localhost.localdomain>
2019-04-15 18:35:24 +0100

    replace watson url

--- src/Pages/Chat/chat.js
+++ src/Pages/Chat/chat.js
@@ -7,7 +7,7 @@ export default class Chat extends Component {
         return(
             <div class="container text-center">
               <img src="../../point_finger.png" alt="pointing finger" id="finger"/>
-              <a href="https://assistant-chat-eu-gb.watsonplatform.net/web/public/3787d149-eeb2-448e-92da-378c4e415335" target="_blank" rel="noopener noreferrer" class="btn btn-info" role="button" id="chat-btn">Launch Chat</a>
+              <a href="https://assistant-chat-us-east.watsonplatform.net/web/public/6fc09082-b569-47dc-952d-60939d13c2ba" target="_blank" rel="noopener noreferrer" class="btn btn-info" role="button" id="chat-btn">Launch Chat</a>
               <p className="chat-label">The button launches a PREVIEW of the Virtual Assistant in  a new tab or window (depending on your browser configuration)</p>
             </div>
         );
